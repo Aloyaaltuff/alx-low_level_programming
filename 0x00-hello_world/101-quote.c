fb62f00b2a31404bc5021119374948dcc14a296d
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-09-07 15:59:37 -0700

    k

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,14 @@
  *main - Entry point
  *
  * Description: print a qoute using write function
- *   write(int fd,const void *buf,size_t cont)
- * Return : 1 (not success)
+ *  size_t write(int fd,const void *buf,size_t cont);
+ *
+ * Return : 1 (error)
 */
 
+
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1,quo,59);
-		return (0);
+{       char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
+		return (1);
 }
