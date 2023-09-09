0d7e72f0916ad8df369041e5e15ec1bcadbc9cf8
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-09-09 16:48:33 -0700

    lkl

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,17 +13,18 @@ int main(void)
 	char ch = 'a';
 	char CH = 'A';
 	/*print a -z*/
-	while (ch <= 'z';
-			{
-			putchar(ch);
-			ch++;
-			}
-			/*print A - Z*/
-			while (CH <= 'Z')
-			{
-			putchar(CH);
-			CH++;
-			}
-			putchar('\n');
-			return (0);
-			}
+	while (ch <= 'z');
+	{
+		putchar(ch);
+		ch++;
+	}
+	
+	/*print A - Z*/
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
