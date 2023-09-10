4aafcc20a38478a2db1cc3fbcd0f855fd4a13808
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-09-09 17:49:54 -0700

    ;k

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabet
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}
