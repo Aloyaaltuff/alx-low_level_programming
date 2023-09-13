f89b0a84c41d094a34a11fa85017087df7dbc7a5
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-09-13 04:43:46 -0700

    lka

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - entrey point
+ *
+ * Description: print putchar
+ *
+ * Return: Always 0 (succsess)
+ *
+*/
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
