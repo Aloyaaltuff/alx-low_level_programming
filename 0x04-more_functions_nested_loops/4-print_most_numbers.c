35e15dce4c85b99d96219e70d3c8d30ac360a32f
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-09-14 14:47:59 -0700

    ;

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #includ "main.h"
 
 /**
- * print_most_numbers - print 0 - 9 ecsept 2 and 4
+ * print_most_numbers - print 0 - 9 apart from 2 and 4
+ * and you can only use _putchar twice
  *
  * Return: Always 0 (success)
 */
