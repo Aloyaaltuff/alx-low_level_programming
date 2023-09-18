89029d94771d6912f06fe185ec6eef0e09ee7bcb
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-09-18 09:45:52 -0700

    kk

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * rest_to_98 - takes pointer to an int parameter and update the valueto 98
+ *
+ * @c: int parameter
+ *
+ * Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
