81cf34e35f8d189f384c712a12d6cc849c225ade
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-09-18 13:03:14 -0700

    l

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,7 @@
 /**
  * *_strcpy - copies the string pointed to by src
  * @dest: char type string
- * @stc: char type string
+ * @src: char type string
  * Description: copy the string pointed to by 'dest'
  * Return: pointer to dest
 */
