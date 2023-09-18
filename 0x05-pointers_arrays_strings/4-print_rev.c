5e9396970492381db916ae9bf080ffa9f3ece45f
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-09-18 11:49:07 -0700

    '

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: pointer for string to print
+ *
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n')
+}
