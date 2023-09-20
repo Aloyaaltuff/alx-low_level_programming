5da39c2b3de5d4b0641380a0c601ec00dfe43bd6
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-09-20 11:36:55 -0700

    oo

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @s: a given string
+ *
+ * Return: a pointer to the string
+ */
+char *cap_string(char *s)
+{
+    int i = 0, j;
+    char sepr[] = " ,;.!?\"()\t{}\n";
+
+    if (s[0] >= 'a' && s[0] <= 'z')
+        s[0] -= 32;
+
+    while (s[i])
+    {
+        j = 0;
+
+        while (sepr[j])
+        {
+            if (s[i] == sepr[j])
+            {
+                if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+                    s[i + 1] -= 32;
+                break;
+            }
+            j++;
+        }
+        i++;
+    }
+    return (s);
+}
