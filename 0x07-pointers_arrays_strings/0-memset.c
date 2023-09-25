5ccc1e2016ce649237434d96bed54ad264317920
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-09-25 11:45:20 -0700

    ;

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** *_memset - function that fills memory with constant byte
+/**
+ * *_memset - function that fills memory with constant byte
  * @s: pointer to put the constsnt
  * @b: constsnt
  * @n: max byte to use
@@ -15,6 +16,5 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[itr] = b;
 	}
-	
 	return (s);
 }
