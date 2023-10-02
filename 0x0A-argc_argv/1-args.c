f689b658baa79e68169f2f2be7f5ac96facce49a
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-10-02 09:41:26 -0700

    l

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - program that print the number of the arguments
+ * @argc: integer
+ * @argv: the value of the argument
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
