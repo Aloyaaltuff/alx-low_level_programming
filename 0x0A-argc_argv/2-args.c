13e5901975cd9ce73f1759c1355f8f1528ee30b2
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-10-02 10:34:23 -0700

    ;

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - programm print all argument
+ * @argc: integer
+ * @argv: value of argument
+ * Return:0
+*/
+int main(int argc, char const *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("argv[%d] is %s\n", i, argv[i]);
+	}
+	return (0);
+}
