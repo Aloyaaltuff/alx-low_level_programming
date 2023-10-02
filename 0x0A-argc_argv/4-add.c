367060e6793a65f505dfe15e062104435d82a477
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-10-02 12:45:58 -0700

    '

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,4 +3,23 @@
 
 /**
  * main - add positive numbers
- * @argc:
+ * @argc: number of argument
+ * @argv: pointer of an array
+ * Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int mull = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '\0' || *c > '9')
+				return (printf("Error\n"), 1)
+					mull += atoi(argv[argc]);
+	}
+	printf("%d\n", mull);
+	return (0);
+}
