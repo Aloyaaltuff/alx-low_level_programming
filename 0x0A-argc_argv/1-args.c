aba8467084149eafe2e13ea93de8e1811c79a5f6
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-10-02 10:09:54 -0700

    l

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,8 @@
  * Return: 0
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]__attribute__((unused)))
 {
-	printf("argc = %d\n", argc);
+	printf("argc = %d\n", argc - 1);
 	return (0);
 }
