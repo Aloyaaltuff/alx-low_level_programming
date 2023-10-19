2dfe960c9a9a9319a9948ff6e8b5ce9de85db4db
Aloyaaltuff <Aloyaaltuff@gmail.com>
2023-10-19 07:20:58 -0700

    oo

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * print_list - function prints all the elements.`
+ * @str: pointer
+ * Return: 0 if str is NULL
+*/
+struct size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strlen - return the length of a string
+ * @s: the string whose length to chekk
+ *
+ * Return: integer length of string
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
